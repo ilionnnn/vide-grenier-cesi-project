83b18fc5dc6d465ba4a4b97684ed6a7d1e441c59
Ilian <ilianmahzem@orange.fr>
2025-06-03 23:16:08 +0200

    fix index.php

--- App/index.php
+++ App/index.php
@@ -1,7 +1,16 @@
 <?php
+
+// Charger la config
 require_once __DIR__ . '/Config.php';
 
-require_once __DIR__ . '/../Core/Router.php'; 
+// Charger le routeur
+require_once __DIR__ . '/../Core/Router.php';
 
-$router = new Core\Router();
-$router->dispatch($_SERVER['REQUEST_URI']);
+// Démarrer le routage
+try {
+    $router = new Core\Router();
+    $router->dispatch($_SERVER['REQUEST_URI']);
+} catch (Exception $e) {
+    // Gérer les erreurs (affichage simple ici)
+    echo "Erreur : " . $e->getMessage();
+}
