467dc39adc0fb9ef0717db70bf1ca4311db47271
ilion <ilianmahzem@orange.fr>
2025-06-29 19:51:31 +0200

    test multi

--- App/Config.php
+++ App/Config.php
@@ -4,19 +4,29 @@ namespace App;
 
 class Config
 {
+    // Valeurs par défaut, fallback si variable d'env absente
     const DB_HOST = 'db';
     const DB_NAME = 'mon_site';
     const DB_USER = 'user';
     const DB_PASSWORD = 'pass';
     const SHOW_ERRORS = true;
     const COOKIE_USER = 'user';
-    const COOKIE_DEFAULT_EXPIRY = 3600 * 24 * 30; 
+    const COOKIE_DEFAULT_EXPIRY = 3600 * 24 * 30;
 
-    public static function get($key) {
+    public static function get($key)
+    {
+        // Essaye d'abord de récupérer la variable d'environnement Docker correspondante
+        $envValue = getenv($key);
+        if ($envValue !== false) {
+            return $envValue;
+        }
+
+        // Sinon, retourne la constante définie dans la classe
         $const = __CLASS__ . '::' . $key;
         if (defined($const)) {
             return constant($const);
         }
+
         return null;
     }
 }
